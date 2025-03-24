67d899d911c0569a9f9d6c590b384c1a791dfffd
Matty Hatton <94123416+Matthew-Hatton@users.noreply.github.com>
2025-03-24 10:28:57 +0000

    Add files via upload

---
+++ R Scripts/6.1 BARENTS_SEA PLOT.R
@@ -0,0 +1,20 @@
+
+## Visualise summarised nemo-medusa model output
+
+#### Set up ####
+
+rm(list=ls(all.names = TRUE))                                               # Wipe the brain
+
+packages <- c("tidyverse", "nemoRsem", "furrr", "tictoc")                       # List packages
+lapply(packages, library, character.only = TRUE)                                # Load packages
+plan(multisession)                                                          # Instructions for parallel processing
+
+TS <- readRDS("./Objects/TS_Barents_Sea.rds") #%>% 
+#  filter(Year < 2051)
+
+ggplot() +
+  #geom_smooth(data = TS,aes(x = date,y = Temperature_avg)) +
+  geom_line(data = TS,aes(x = date,y = Temperature_avg,color = SSP)) +
+  facet_wrap(~Compartment,ncol = 1) +
+  labs(title = "Barents_Sea Temperature avg")
+  NULL
